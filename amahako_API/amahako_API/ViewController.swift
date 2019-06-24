0005737708104fa390fb098d6cb044b07606ace9
yut0124 <b1018133@fun.ac.jp>
2019-06-24 17:48:45 +0900

    6/24の進歩　TableViewを実装できただけ

--- amahako_API/amahako_API/ViewController.swift
+++ amahako_API/amahako_API/ViewController.swift
@@ -8,13 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    let LIST = ["0","1","2"]
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return LIST.count
+    }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel!.text = LIST[indexPath.row]
+        return cell
+    }
 
 }
 
